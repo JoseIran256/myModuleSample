5d5d260a5781141c5c60a8a6f76b7e5838e9b686
JoseIran256 <joseiran2109@gmail.com>
2025-11-24 20:44:51 -0300

    Modificações na aula do professor dia 24.11.25

--- teste.js
+++ teste.js
@@ -1,5 +1,5 @@
-import lib from "./mylib.js"
+import lib from "@joseiran/svguid-a3"
 
 // String contendo SVG criado
-let svgText = lib.getSVG("ana123")
+let svgText = lib.getSVG("a")
 console.log(svgText);
